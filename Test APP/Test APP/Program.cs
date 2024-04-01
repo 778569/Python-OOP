edde954e5a041ba52838d2d99453413101f2e9d9
Kavinda Bandara <52319671+778569@users.noreply.github.com>
2024-04-01 23:58:57 +0530

    Add files via upload

---
+++ Test APP/Test APP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+using Test_APP;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+
+        RectangleR rectangleR = new RectangleR(10,20);
+        //rectangleR.FindArea();
+        rectangleR.printDetails();
+
+        Console.ReadLine();
+
+    }
+}
---
+++ Test APP/Test APP/RectangleR.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_APP
+{
+    public class RectangleR
+    {
+
+        public int width = 0;
+        public int height = 0;
+        public int area = 0;
+        public int perimeter = 0;
+
+        public int count = 0;
+
+       
+        public RectangleR(int Width = 0, int Height = 0)
+        {
+            this.width = Width;
+            this.height = Height;
+            //this.area = Area;
+            //this.perimeter = Parameter;
+        }
+
+        public int FindArea()
+        {
+            area = width * height;
+            return area;
+        }
+
+        public int finePerimeter()
+        {
+            var oneSidelength = height + width;
+            perimeter = 2 * oneSidelength;
+            return perimeter;
+        }
+
+        public void printDetails()
+        {
+            Console.WriteLine("width : " + width );
+            Console.WriteLine("height : " + height);
+
+            Console.WriteLine("area : " + area);
+
+            Console.WriteLine("parimeter : " + perimeter);
+
+            Console.WriteLine("count : " + count);
+
+        }
+
+    }
+}
